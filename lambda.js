42e8805442a821b10a8d88e14712a4f17d5e24c9
Christian Fei <crifei93@gmail.com>
2016-09-24 02:40:45 +0200

    add boilderplate StatusRepository and other stuff i don't remember

--- lambda.js
+++ lambda.js
@@ -1,4 +1,19 @@
 'use strict'
-exports.handler = function(event, context) {
-  context.succeed('hello world')
+const request = require('request')
+exports.handler = (event, context) => {
+  const requestParams = {
+    uri: event.url,
+    method: event.method || 'get',
+    headers: event.headers || {},
+    time: true
+  }
+  console.log('-- requestParams', requestParams)
+  request(requestParams, (err, response) => {
+    const status = {
+      url: event.url,
+      statusCode: response.statusCode,
+      loadingTime: response.elapsedTime
+    }
+    console.log('-- status', status)
+  })
 }
