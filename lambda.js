9f80f7a0f49806766ffdfbe094c4847b1f917abc
Christian Fei <crifei93@gmail.com>
2016-09-26 11:59:25 +0200

    increase timeout, improve error handling

--- lambda.js
+++ lambda.js
@@ -1,6 +1,7 @@
 'use strict'
 const request = require('request')
 const StatusRepository = require('./lib/StatusRepository')
+const SECONDS = 1 * 1000
 
 exports.handler = (event, context) => {
   const url = event.url
@@ -8,16 +9,20 @@ exports.handler = (event, context) => {
     uri: url,
     method: event.method || 'get',
     headers: event.headers || {},
-    time: true
+    time: true,
+    timeout: 60*SECONDS
   }
   console.log('-- requestParams', requestParams)
   return request(requestParams, (err, response) => {
-    const status = {
-      id: Date.now(),
-      url: url,
-      statusCode: response.statusCode,
-      loadingTime: response.elapsedTime
+    if( err || !response ) {
+      console.log('-- err, response', err, response)
+      throw err
     }
+    console.log('-- response defined', !!response)
+    const id = Date.now()
+    const statusCode = response.statusCode
+    const loadingTime = response.elapsedTime
+    const status = {id,url,statusCode,loadingTime}
     console.log('-- status', status)
     StatusRepository.insert(status)
     .then(context.succeed)
