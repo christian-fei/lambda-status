291dce795ea8d04efc2b7f158512f41460181803
Christian Fei <crifei93@gmail.com>
2016-09-28 00:17:03 +0200

    made commands more humand friendly

--- bin/create-table.js
+++ bin/create-table.js
@@ -1,11 +1,33 @@
 'use strict'
 
+const exec = require('child_process').exec
+
 module.exports = {
   execute: execute
 }
 
-function execute() {
+
+function execute(tableName) {
+  const command = '\
+  aws dynamodb create-table \
+  --region eu-central-1 \
+  --table-name '+ tableName +' \
+  --attribute-definitions AttributeName=id,AttributeType=N \
+  --key-schema AttributeName=id,KeyType=HASH \
+  --provisioned-throughput \
+  ReadCapacityUnits=1,WriteCapacityUnits=1 \
+  --query TableDescription.TableArn \
+  --output text'
   return new Promise((resolve, reject) => {
-    resolve()
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        console.log(`stderr: ${stderr}`)
+        return reject(error)
+      }
+      console.log(`==> Successfully created table: ${tableName}`)
+      console.log(`==> stdout: ${stdout}`)
+      return resolve(stdout)
+    })
   })
 }
--- bin/test.js
+++ bin/test.js
@@ -12,9 +12,9 @@ describe('installer', () => {
     const mockCreateTable = sinon.mock(createTable)
     const mockOutro = sinon.mock(outro)
     mockGreeter.expects('execute').returns(Promise.resolve())
-    mockChooseTableName.expects('execute').returns(Promise.resolve('test-status'))
-    mockCreateTable.expects('execute').returns(Promise.resolve('test-status'))
-    mockOutro.expects('execute').returns(Promise.resolve('test-status'))
+    mockChooseTableName.expects('execute').returns(Promise.resolve('test-table'))
+    mockCreateTable.expects('execute').returns(Promise.resolve())
+    mockOutro.expects('execute').returns(Promise.resolve())
 
     return installer.start()
 
