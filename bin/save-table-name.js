d9710a9ec0074409f85223356ff1be2ff63a362b
Christian Fei <crifei93@gmail.com>
2016-09-28 00:28:20 +0200

    add save-table-name action

--- bin/save-table-name.js
+++ bin/save-table-name.js
@@ -1,11 +1,24 @@
 'use strict'
 
+const exec = require('child_process').exec
+
 module.exports = {
   execute: execute
 }
 
-function execute() {
+function execute(tableName) {
   return new Promise((resolve, reject) => {
-    resolve()
+    const command = `sed -i -e "s/'status'/'${tableName}'/g" ../lib/StatusRepository.js`
+    exec(command, (error, stdout, stderr) => {
+      console.log(`stdout: ${stdout}`)
+      if (error) {
+        console.error(`exec error: ${error}`)
+        console.log(`stderr: ${stderr}`)
+        return reject(error)
+      }
+      return resolve(tableName)
+    })
   })
 }
+
+
