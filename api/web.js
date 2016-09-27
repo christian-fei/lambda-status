84459b55c1f03edeed7c37a935d8e26d08401fc2
Christian Fei <crifei93@gmail.com>
2016-09-27 05:30:08 +0200

    snapshot

---
+++ api/web.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const ApiBuilder = require('claudia-api-builder')
+const api = new ApiBuilder()
+const StatusRepository = require('./lib/StatusRepository')
+
+module.exports = api
+
+api.get('/', function (request) {
+  return 'Ok'
+})
+
+api.get('/statuses', function (request) {
+  console.log('-- request', request)
+  const from = parseFrom(request.queryString.from)
+  return StatusRepository.latest(from)
+})
+
+
+const HOUR = 1000*60*60
+const DAY = HOUR*24
+const WEEK = DAY*7
+
+function parseFrom(from) {
+  if(!from) {
+    return
+  }
+  if(/\d+hago/.test(from)) {
+    const hoursAgo = from.match(/(\d)+hago/)[1]
+    return Date.now()-HOUR*hoursAgo
+  }
+  if(/\d+dago/.test(from)) {
+    const daysAgo = from.match(/(\d)+dago/)[1]
+    return Date.now()-DAY*daysAgo
+  }
+  if(/\d+wago/.test(from)) {
+    const weeksAgo = from.match(/(\d)+wago/)[1]
+    return Date.now()-WEEK*weeksAgo
+  }
+}
