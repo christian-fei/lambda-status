78c315955978449294ed220dfb35e9bc47466e71
Christian Fei <crifei93@gmail.com>
2016-09-28 00:39:06 +0200

    fix installer

--- bin/save-table-name.js
+++ bin/save-table-name.js
@@ -8,7 +8,7 @@ module.exports = {
 
 function execute(tableName) {
   return new Promise((resolve, reject) => {
-    const command = `sed -i "" -e "s/'status'/'${tableName}'/g" ../lambda/lib/StatusRepository.js; sed -i "" -e "s/'status'/'${tableName}'/g" ../api/lib/StatusRepository.js`
+    const command = `sed -i "" -e "s/'status'/'${tableName}'/g" ./lambda/lib/StatusRepository.js; sed -i "" -e "s/'status'/'${tableName}'/g" ./api/lib/StatusRepository.js`
     exec(command, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`)
