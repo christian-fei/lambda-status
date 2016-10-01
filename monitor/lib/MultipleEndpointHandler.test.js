3ec2676d873d66089497981b45fac80dd967a35b
Christian Fei <crifei93@gmail.com>
2016-10-01 22:02:10 +0200

    refactor tests

--- monitor/lib/MultipleEndpointHandler.test.js
+++ monitor/lib/MultipleEndpointHandler.test.js
@@ -2,28 +2,29 @@
 
 const MultipleEndpointHandler = require('./MultipleEndpointHandler')
 const SingleEndpointHandler = require('./SingleEndpointHandler')
-const StatusRepository = require('./StatusRepository')
-const RequestService = require('./RequestService')
 const LambdaService = require('./LambdaService')
 
-const event = {
+
+
+const testRequestService = {request: () => Promise.resolve({statusCode: 200, elapsedTime: 50})}
+const testStatusRepository = {insert: () => Promise.resolve()}
+
+const testEvent = {
   "base": "https://example.org",
   "endpoints": ["/", "/other"]
 }
-
-const context = {done:() => Promise.resolve(), succeed:() => Promise.resolve(), fail:() => Promise.resolve()}
-
-const dummyRequestService = {request: () => Promise.resolve({statusCode: 200, elapsedTime: 50})}
-const dummyStatusRepository = {insert: () => Promise.resolve()}
+const testContext = {done:() => Promise.resolve(), succeed:() => Promise.resolve(), fail:() => Promise.resolve()}
 
 describe('MultipleEndpointHandler', () => {
   it('invokes lambda', () => {
     const lambdaService = sinon.mock(new LambdaService())
-    lambdaService.expects('invoke').returns(Promise.resolve())
+    const mockSingleEndpointHandler = sinon.mock(SingleEndpointHandler)
+    const nextEvent = { base: "https://example.org", endpoints: ["/"] }
+    lambdaService.expects('invoke').withArgs(nextEvent, testContext).returns(Promise.resolve())
+    mockSingleEndpointHandler.expects('handler').returns(Promise.resolve())
 
-    return MultipleEndpointHandler.handler(event, context, dummyRequestService, dummyStatusRepository, lambdaService.object)
-    .then(() => {
-      lambdaService.verify()
-    })
+    return MultipleEndpointHandler.handler(testEvent, testContext, testRequestService, testStatusRepository, lambdaService.object)
+    .then(() => lambdaService.verify())
+    .then(() => mockSingleEndpointHandler.verify())
   })
 })
