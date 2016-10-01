f87f5878f6ae40b0d6f15a9eff450e38ed4708ac
Christian Fei <crifei93@gmail.com>
2016-10-01 23:15:17 +0200

    change project name task

--- bin/save-project-name.js
+++ bin/save-project-name.js
@@ -10,7 +10,7 @@ function execute(projectName) {
   return new Promise((resolve, reject) => {
     const monitorName = `${projectName}-monitor`
     const apiName = `${projectName}-api`
-    const command = `sed -i "" -e 's/"lambda-status-monitor"/"${projectName}"/g' ./monitor/package.json; sed -i "" -e 's/"lambda-status-api"/"${projectName}"/g' ./api/package.json`
+    const command = `sed -i "" -e 's/"lambda-status-monitor"/"${projectName}-monitor"/g' ./monitor/package.json; sed -i "" -e 's/"lambda-status-api"/"${projectName}-api"/g' ./api/package.json`
     exec(command, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`)
